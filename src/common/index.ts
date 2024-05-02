9f8ccd19843cbb3c9ab939e1aa01ea2ccb98b46c
rainlotus97 <s0704127x@163.com>
2024-05-02 19:19:19 +0800

    增加注释

--- src/common/index.ts
+++ src/common/index.ts
@@ -3,6 +3,7 @@ export interface SalaryState {
     salary: number;
 }
 
+// 根据字符串转化为日期进行排序
 export const calculateDateSort = (date: string) => {
     const parts = date.split('年').join('-').split('月').join('-').split('日').join('').split('-');
     return new Date(parseInt(parts[0]), parseInt(parts[1]) - 1, parseInt(parts[2]));
