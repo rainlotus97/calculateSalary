8dd39b6a7ccc59a97f17d294323dbfcbd6f95dc1
rainlotus97 <68591650+rainlotus97@users.noreply.github.com>
2024-05-02 11:49:35 +0000

    Deploying to gh-pages from  @ 5cf17ab7e2fec21bc564ff0060e0b0f75c937978 🚀

--- src/stores/index.ts
+++
@@ -1,9 +0,0 @@
-import { createPinia } from 'pinia'
-//pinia持久化插件
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-
-const store = createPinia()
-store.use(piniaPluginPersistedstate)
-
-export default store
-
